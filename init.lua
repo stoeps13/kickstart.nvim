62a2ad739f4059caf9bd73051a76403d8eddb394
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-05-22 22:43:49 +0200

    setup snacks

--- init.lua
+++ init.lua
@@ -1164,18 +1164,15 @@ require('lazy').setup({
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
-      bigfile = { enabled = true },
       dashboard = { enabled = true },
       explorer = { enabled = true },
       indent = { enabled = true },
       input = { enabled = true },
+      lazygit = { enabled = true },
       picker = { enabled = true },
       notifier = { enabled = true },
       quickfile = { enabled = true },
       scope = { enabled = true },
-      scroll = { enabled = true },
-      statuscolumn = { enabled = true },
-      words = { enabled = true },
     },
   },
   -- Vimwiki, daily notes, TIL
